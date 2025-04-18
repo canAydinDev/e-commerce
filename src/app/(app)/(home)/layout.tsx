202f8cf88ecf59844da89d1c30ee27d5d486c4a5
canaydindev <canaydindev@gmail.com>
2025-04-18 17:46:51 +0300

    04: payload integration

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -9,7 +9,7 @@ const Layout = ({children}:LayoutProps)=> {
 return (
     <div className="flex flex-col min-h-screen">
         <Navbar/>
-        <div className="flex-1 bg-[#F4F4F0]">
+        <div className="flex-1 bg-[#F4F4F0      ]">
         {children}
         </div>
         <Footer/>
