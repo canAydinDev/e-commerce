d3b0c5f811f51dd96a4b73bf4b2585997a89265e
canaydindev <canaydindev@gmail.com>
2025-04-20 15:03:16 +0300

    05: search filters

--- src/collections/Categories.ts
+++ src/collections/Categories.ts
@@ -9,5 +9,29 @@ export const Categories: CollectionConfig = {
       type: "text",
       required: true,
     },
+    {
+      name: "slug",
+      type: "text",
+      unique: true,
+      index: true,
+      required: true,
+    },
+    {
+      name: "color",
+      type: "text",
+    },
+    {
+      name: "parent",
+      type: "relationship",
+      relationTo: "categories",
+      hasMany: false,
+    },
+    {
+      name: "subcategories",
+      type: "join",
+      collection: "categories",
+      on: "parent",
+      hasMany: true,
+    },
   ],
 };
